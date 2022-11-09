68b73cde5b5bdbad2f1d0d2531367c7cc945a512
Ali Karami <kaali728@yahoo.com>
2022-11-09 19:36:07 +0100

    fix droped off to dropped off

--- frontend/lib/models/assetEnum.ts
+++ frontend/lib/models/assetEnum.ts
@@ -11,7 +11,7 @@ export enum ASSET_PICK_DROP {
   PICKUP = "Pick up",
   DROP_OFF = "Drop off",
   PICKEDUP = "Picked up",
-  DROPEDOFF = "Droped off",
+  DROPEDOFF = "Dropped off",
   ASSET_CREATED = "Asset created",
   AWAITING_PAYMENT = "Awaiting Payment",
 }
